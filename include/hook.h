b89676ba599307591346f101fe6bab66045c95de
xzmjx <2013257123@qq.com>
2022-04-06 19:23:52 +0800

    hook

--- include/hook.h
+++ include/hook.h
@@ -12,11 +12,32 @@ void EnableHook();
 }///namespace xzmjx
 
 extern "C"{
+///sleep
 typedef unsigned int (*sleep_fun)(unsigned int seconds);
 extern sleep_fun sleep_f;
 
 typedef int (*usleep_fun)(useconds_t seconds);
 extern usleep_fun usleep_f;
+
+typedef int (*nanosleep_fun)(const struct timespec *req, struct timespec *rem);
+extern nanosleep_fun nanosleep_f;
+
+///socket
+typedef int (*socket_fun)(int domain,int type,int protocol);
+extern socket_fun socket_f;
+
+///read
+typedef ssize_t (*read_fun)(int fd, void *buf, size_t count);
+extern read_fun read_f;
+
+
+
+///write
+
+
+///others
+
+
 
 }
 
