40711031634b408cc9497512f34407e10dbd2333
xzmjx_lenovo <2013257123@qq.com>
2022-05-10 22:22:31 +0800

    http session

--- include/http/http_session.h
+++ include/http/http_session.h
@@ -4,5 +4,20 @@
 
 #ifndef XZMJX_HTTP_SESSION_H
 #define XZMJX_HTTP_SESSION_H
+#include "socketstream.h"
+#include "http/http.h"
 
+namespace xzmjx{
+namespace http{
+class HttpSession : public SocketStream{
+public:
+    typedef std::shared_ptr<HttpSession> ptr;
+    HttpSession(Socket::ptr socket,bool owner = true);
+
+    HttpRequest::ptr recvRequest();
+
+    int sendResponse(HttpResponse::ptr rsp);
+};
+}
+}
 #endif //XZMJX_HTTP_SESSION_H
--- src/http/http_session.cpp
+++ src/http/http_session.cpp
@@ -1,4 +1,55 @@
 //
 // Created by 20132 on 2022/5/7.
 //
+#include "http/http_session.h"
+#include "log.h"
+#include "http/http_parser.h"
 
+static xzmjx::Logger::ptr g_logger = XZMJX_LOG_NAME("system");
+namespace xzmjx{
+namespace http{
+HttpSession::HttpSession(Socket::ptr socket,bool owner)
+    : SocketStream(socket,owner){
+
+}
+
+HttpRequest::ptr HttpSession::recvRequest(){
+    HttpRequestParser::ptr parser(new HttpRequestParser);
+    uint64_t buff_size = HttpRequestParser::GetHttpRequestBufferSize();
+    char* buf = (char*)malloc(buff_size);
+    uint64_t offset = 0; // buf中现有数据量
+    do{
+        int len = read(buf+offset,buff_size-offset);
+        if(len<=0){
+            close();
+            return nullptr;
+        }
+        len+=offset;
+        uint64_t n_parser = parser->execute(buf,len);
+        if(parser->hasError()){
+            close();
+            return nullptr;
+        }
+        offset = len - n_parser;
+        if(offset>=buff_size){
+            close();
+            return nullptr;
+        }
+        if(parser->isFinish()){
+            break;
+        }
+    }while(true);
+    HttpRequest::ptr req = parser->getData();
+    req->init();
+    free(buf);
+    return req;
+}
+
+int HttpSession::sendResponse(HttpResponse::ptr rsp){
+    std::stringstream ss;
+    rsp->dump(ss);
+    std::string data = ss.str();
+    return writeFixedSize(&data[0],data.size());
+}
+}
+}
